0e7754af115fb879ac5683780db4f3c652cf9ea6
Patrick Buco <patrickbuco11@gmail.com>
2022-10-11 19:50:37 +0800

    ADD js file

--- resources/views/user-interface/classic.blade.php
+++ resources/views/user-interface/classic.blade.php
@@ -1,14 +1,65 @@
 @extends('layouts.app', ['title' => 'Classic'])
 
 @section('styles')
-    {{-- CSS Here --}}
+<link rel="stylesheet" href="{{ asset('css/classic/style.css') }}">
 @endsection
 
 @section('content')
-{{-- HTML here --}}
-<p>Classic</p>
+<section>
+    <div class="timer">
+        <span id="timer">1:30</span>
+    </div>
+    <div class="equation-container"></div>
+    <div class="reset">
+        <button>Reset</button>
+    </div>
+</section>
 @endsection
 
 @section('scripts')
-<script src=""></script>
+<script type="module">
+    import {equation, timer} from '/js/classic/utils.js'
+    (() => {
+
+        let successFX = 'animated bounce', failedFX = 'animated headShake', fxEnds = 'animationend AnimationEnd mozAnimationEnd webkitAnimationEnd';
+
+        //initialize timer
+        timer.init();
+        equation.generateDOM()
+
+        $(document).on('click', 'div.equation', function () {
+            if($(this).hasClass('active')){
+                return
+            }
+            $(this).addClass('active');
+            console.log($(this).data('answer'))
+            equation.setAnswer($(this).data('answer'))
+
+            if(equation.answers.length == 4){
+                if(equation.isAnswerSorted()){
+                    $('span#timer').addClass(`text-success ${successFX}`).one(fxEnds, function(){
+                        $('span#timer').removeClass(`text-success ${successFX}`);
+                    });
+                    timer.addTime(3);
+                    equation.generateDOM()
+                }else{
+                    $('span#timer').addClass(`text-danger ${failedFX}`).one(fxEnds, function(){
+                        $('span#timer').removeClass(`text-danger ${failedFX}`);
+                        $('div.equation').removeClass('active');
+                    });
+                    timer.deductTime(3);
+                    equation.answers = []
+                    
+                }
+            }
+        });
+
+        $('.reset button').click(function (e) { 
+            e.preventDefault();
+            $('div.equation').removeClass('active');
+            equation.answers = []
+        });         
+
+    })();
+</script>
 @endsection
