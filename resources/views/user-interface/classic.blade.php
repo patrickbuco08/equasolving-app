967c68c4aedc486c700daf16b0dc5e5aa76e3454
russell35 <russellarondelarosa@gmail.com>
2022-10-31 12:14:49 +0800

    push

--- resources/views/user-interface/classic.blade.php
+++ resources/views/user-interface/classic.blade.php
@@ -6,8 +6,22 @@
 
 @section('content')
 <section>
+    <div class="menu-container">
+        <div class="menu">
+            <button id="pause">pause</button>
+        </div>
+        <div class="menu">
+            <button id="continue">continue</button>
+        </div>
+        <div class="menu">
+            <button id="quit">quit</button>
+        </div>
+    </div>
+    <div class="level">
+        <span id="level">Level: 1</span>
+    </div>
     <div class="timer">
-        <span id="timer">1:30</span>
+        <span id="timer"></span>
     </div>
     <div class="equation-container"></div>
     <div class="reset">
@@ -18,30 +32,40 @@
 
 @section('scripts')
 <script type="module">
-    import {equation, timer} from '/js/classic/utils.js'
+    import {equation, timer, gameTimer} from '/js/classic/utils.js'
     (() => {
 
         let successFX = 'animated bounce', failedFX = 'animated headShake', fxEnds = 'animationend AnimationEnd mozAnimationEnd webkitAnimationEnd';
 
         //initialize timer
         timer.init();
-        equation.generateDOM()
-
+        gameTimer.init();
+        equation.generateDOM();
+        let i = 0;
         $(document).on('click', 'div.equation', function () {
-            if($(this).hasClass('active')){
+            
+            if($(this).hasClass('active') && $(this).hasClass(''+i-1+'')){
+                i--;
+                $('div.equation.'+''+i).removeClass('active');
+                $('div.equation').removeClass(''+i+'');
+                equation.answers.splice(i);
+                return
+            }
+            else if($(this).hasClass('active') || $(this).hasClass('0 1 2 3')){
                 return
             }
             $(this).addClass('active');
-            console.log($(this).data('answer'))
+            $(this).addClass(''+i+'');
             equation.setAnswer($(this).data('answer'))
-
+            i++;
             if(equation.answers.length == 4){
                 if(equation.isAnswerSorted()){
                     $('span#timer').addClass(`text-success ${successFX}`).one(fxEnds, function(){
                         $('span#timer').removeClass(`text-success ${successFX}`);
                     });
-                    timer.addTime(3);
+                    timer.addTime(5);
                     equation.generateDOM()
+                    i = 0;
                 }else{
                     $('span#timer').addClass(`text-danger ${failedFX}`).one(fxEnds, function(){
                         $('span#timer').removeClass(`text-danger ${failedFX}`);
@@ -49,17 +73,32 @@
                     });
                     timer.deductTime(3);
                     equation.answers = []
+                    $('div.equation').removeClass('0 1 2 3');
+                    i = 0;
                     
-                }
+                } 
             }
         });
 
         $('.reset button').click(function (e) { 
             e.preventDefault();
             $('div.equation').removeClass('active');
+            $('div.equation').removeClass('0 1 2 3');
             equation.answers = []
+            i = 0;
         });         
-
+        $('.menu #pause').click(function (e) { 
+            e.preventDefault();
+            timer.pause();
+        });   
+        $('.menu #continue').click(function (e) { 
+            e.preventDefault();
+            timer.continue();
+        }); 
+        $('.menu #quit').click(function (e) { 
+            e.preventDefault();
+            timer.quit();
+        }); 
     })();
 </script>
 @endsection
